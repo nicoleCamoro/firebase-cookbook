82d70201558e0c4f66be8264c4d795c6649cc5c0
Franchette Camoro <franchette@symph.co>
2020-03-28 09:25:06 +0800

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Routes from "./routes";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-    return (
-        <div className="App">
-            <Routes />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
