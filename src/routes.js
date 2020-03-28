6fac57157f4463208c13eb9ed8d47e7e4f99f9c5
Franchette Camoro <franchette@symph.co>
2020-03-28 11:11:01 +0800

    Add navbar

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
 import { createBrowserHistory } from "history";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+
 import Login from "./component/Login";
+import Recipes from "./component/Recipes";
 
 class Routes extends Component {
   render() {
     return (
       <Router history={createBrowserHistory}>
+        <Navbar expand="md" className="app-nav">
+          <Navbar.Brand href="/recipes">Firebase cookbook</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link to="/recipes">Home</Link>
+              <Link to="/new-recipe">New Recipes</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Login />
           </Route>
-          <Route path="/recipes">
-            <Login />
+          <Route exact path="/recipes">
+            <Recipes />
           </Route>
-          <Route path="/recipes/:recipe_id">
-            <Login />
+          <Route exact path="/recipes/:recipe_id">
+            <Recipes />
           </Route>
         </Switch>
       </Router>
