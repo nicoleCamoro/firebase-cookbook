a224daed13e3656da85f94c1472a88cbfffe10a7
Franchette Camoro <franchette@symph.co>
2020-03-28 11:11:19 +0800

    Add recipes page

--- src/component/Recipes.js
+++ src/component/Recipes.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
-
+import { withRouter } from "react-router-dom";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
 class Recipes extends Component {
-    render() {
-        return <div>Recipes</div>;
-    }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col sm="12" className="my-3">
+            <h1>Your Recipes</h1>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://via.placeholder.com/150/09f/fff.png"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Button variant="primary">Make this</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default Recipes;
+export default withRouter(Recipes);
